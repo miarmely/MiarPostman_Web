b76449177745199f191fbad2fa210a704a458003
Mert Akdemir <comp.eng.m.akdemir@gmail.com>
2023-07-15 10:59:44 +0300

    Base Web Page Created.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    $("#menu_left li").click(function(){
+        switch($(this).index()){
+            case 0:
+                break;
+                
+            case 1:
+                break;
+
+            case 2:
+                break;
+        
+            case 3:
+                break;
+
+            case 4:
+                break;
+        
+            case 5:
+                break;
+        }
+    });
+});
