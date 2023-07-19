affca7f6f4bc3138cfa4c5c8ea34a07948d923df
Mert Akdemir <comp.eng.m.akdemir@gmail.com>
2023-07-19 15:58:07 +0300

    'Register Date' Feature Added To Get And Post Pages.

--- js/nav_get.js
+++ js/nav_get.js
@@ -1,49 +1,41 @@
 $(function () {
-    function getPersonalById(id, table) {  // get personal by id
-        $.ajax({
-            type: "GET",
-            url: `http://localhost:5282/api/employee/${id}`,
-            dataType: "json",
-            statusCode: {
-                200: function (response) {
-                    // add personels to table
-                    table.append(`<tr><td>${response.id}</td>
-                        <td>${response.fullName}</td>
-                        <td>${response.lastName}</td>
-                        <td>${response.job}</td>
-                        <td>${response.salary}</td><tr>
-                    `);
-                },
-                404: function () {
-                    alert(`Id:${id} Not Found!..`);
-                }
-            }
-        });
+    function dateFormatter(stringDate){
+        let date = new Date(stringDate);  // string convert to Date
+        let dd = date.getDate();
+        let MM = date.getMonth() + 1;
+        let yyyy = date.getFullYear();
+        let HH = date.getHours();
+        let mm = date.getMinutes();
+        
+        return `${dd}.${MM}.${yyyy} - ${HH}:${mm}`;
     }
+
 
     function getPersonals() {
         let id = $("#inpt_id").val().trim() ? $("#inpt_id").val().trim() : -1;
-        let fullName = $("#inpt_fullName").val().trim() ? $("#inpt_fullName").val().trim() : -1
-        let lastName = $("#inpt_lastName").val().trim() ? $("#inpt_lastName").val().trim() : -1
-        let job = $("#inpt_job").val().trim() ? $("#inpt_job").val().trim() : -1
-        let salary = $("#inpt_salary").val().trim() ? $("#inpt_salary").val().trim() : -1
+        let fullName = $("#inpt_fullName").val().trim() ? $("#inpt_fullName").val().trim() : -1;
+        let lastName = $("#inpt_lastName").val().trim() ? $("#inpt_lastName").val().trim() : -1;
+        let job = $("#inpt_job").val().trim() ? $("#inpt_job").val().trim() : -1;
+        let salary = $("#inpt_salary").val().trim() ? $("#inpt_salary").val().trim() : -1;
+        let registerDate = $("#inpt_registerDate").val().trim() ? $("#inpt_registerDate").val().trim() : -1;
         let table = $("#display tbody");
 
         table.empty(); // reset table
 
         $.ajax({
             type: "GET",
-            url: `http://localhost:5282/api/employee?id=${id}&fullName=${fullName}&lastName=${lastName}&job=${job}&salary=${salary}`,
+            url: `http://localhost:5282/api/employee?id=${id}&fullName=${fullName}&lastName=${lastName}&job=${job}&salary=${salary}&registerDate=${registerDate}`,
             datatype: "json",
             statusCode: {
                 200: function (response) {
                     response.forEach(function (personal) {
-                        table.append(
+                        table.prepend(
                             `<tr><td>${personal.id}</td>
                             <td>${personal.fullName}</td>
                             <td>${personal.lastName}</td>
                             <td>${personal.job}</td>
-                            <td>${personal.salary}</td></tr>
+                            <td>${personal.salary} TL</td>
+                            <td>${dateFormatter(personal.registerDate)}</td></tr>
                         `);
                     });
                 },
@@ -53,6 +45,7 @@ $(function () {
             }
         });
     }
+
 
     $("#btn_display").click(function () {
         getPersonals();
--- js/nav_post.js
+++ js/nav_post.js
@@ -1,13 +1,25 @@
-$(function () {
+$(function () {    
+    function dateFormatter(stringDate){
+        let date = new Date(stringDate);  // string convert to Date
+        let dd = date.getDate();
+        let MM = date.getMonth() + 1;
+        let yyyy = date.getFullYear();
+        let HH = date.getHours();
+        let mm = date.getMinutes();
+        return `${dd}.${MM}.${yyyy} - ${HH}:${mm}`;
+    }
+
     $("#input form").submit(function (event) {
         event.preventDefault();  // for to hide parameters on route. 
-
+        
+        // set variables for ajax
         let table = $("#display tbody");
         let formData = {
             "fullName": $("#inpt_fullName").val(),
             "lastName": $("#inpt_lastName").val(),
             "job": $("#inpt_job").val(),
-            "salary": $("#inpt_salary").val()
+            "salary": $("#inpt_salary").val(),
+            "registerDate" : new Date()
         }
 
         $.ajax({
@@ -23,7 +35,8 @@ $(function () {
                         <td>${response.fullName}</td>
                         <td>${response.lastName}</td>
                         <td>${response.job}</td>
-                        <td>${response.salary}</td></tr>`
+                        <td>${response.salary} TL</td>
+                        <td>${dateFormatter(response.registerDate)}</td></tr>`
                     )
                 }
             }
