c7a96507c4e59689b1b122599835c00dbf88fd20
Mert Akdemir <comp.eng.m.akdemir@gmail.com>
2023-07-23 22:30:51 +0300

    Service Of Role Features Connected And Role Features Improved.

--- js/nav_post.js
+++ js/nav_post.js
@@ -1,8 +1,12 @@
 import {dateTimeFormatter} from "./library.js";
 
 $(function () {
+    let roleCounter = 1;  // for to set id of selects
+    let roleLimit = 0;
+    
     fillTable();
-    fillSelect();
+    fillRole1("slct_role1");
+    
 
     function fillTable(){
         let todayDate = new Date().toLocaleDateString();
@@ -20,7 +24,7 @@ $(function () {
                             <td>${personal.fullName}</td>
                             <td>${personal.lastName}</td>
                             <td>${personal.job}</td>
-                            <td>${personal.salary}</td>
+                            <td>${personal.salary} TL</td>
                             <td>${dateTimeFormatter(personal.registerDate)}</td></tr>`)
                     })
                 },
@@ -28,27 +32,52 @@ $(function () {
         })
     }
 
-    function fillSelect(){
-        let roleSelect = $("#slct_role1");
-        roleSelect.append(
-            `<option>Admin</option>
-            <option>Employee</option>
-            <option>Manager</option>`
-        );
+
+    function fillRole1(){
+        let slct_role1 = $(`#slct_role1`);
+        
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:5282/api/role",
+            datatype: "json",
+            statusCode: {
+                200: function(response){  // add roles to Role 1 Combobox
+                    response.forEach(function(role){
+                        slct_role1.append(
+                            `<option>${role.roleName}</option>`
+                        )
+                    })
+
+                    slct_role1.val("");  // set select text as blank
+                    roleLimit = $("#slct_role1 option").length;
+
+                    // enable new role button
+                    $("#btn_newRole").removeAttr("disabled");
+                }      
+            }
+        });    
     }
 
 
     $("#input form").submit(function (event) {  // when save button clicked
-        event.preventDefault();  // for to hide parameters on route.
-        
-        // set variables for ajax
+        event.preventDefault();  // for to hide parameters on route
         let table = $("#display tbody");
+
+        // add roles to list
+        let roles = [];
+        for(let no=1; no<=roleCounter; no+=1)
+            roles.push(
+                $(`#slct_role${no}`).val()
+            );
+
+        // set variables for ajax
         let formData = {
             "fullName": $("#inpt_fullName").val(),
             "lastName": $("#inpt_lastName").val(),
             "job": $("#inpt_job").val(),
             "salary": $("#inpt_salary").val(),
-            "registerDate" : new Date().toLocaleString()
+            "registerDate" : new Date().toLocaleString(),
+            "roles" : roles
         }
 
         $.ajax({
@@ -77,31 +106,49 @@ $(function () {
         });
     })
 
-    let roleCounter = 1;  // for to set id of selects
-    let roleLimit = $("#slct_role1 option").length
+
     $("#btn_newRole").click(function(){
+        // is previous role empty?
+        let lastRoleVal = $(`#slct_role${roleCounter}`).val();
+        if(!lastRoleVal){
+            alert(`You Can't Leave Role ${roleCounter} Blank!`)
+            return;
+        }
+
+        // disable new role button && disable previous role
+        $(`#slct_role${roleCounter}`).attr("disabled", "");
+
         roleCounter+=1;
         let table = $("#input tbody");
         let id = `slct_role${roleCounter}`;
-
+        
+        // add select to table
         table.append(`
             <tr><td>Role ${roleCounter}:</td>
-                <td><select id=${id}>
-                        <option>Admin</option>
-                        <option>Employee</option>
-                        <option>Manager</option>
+                <td><select id=${id} required>
                     </select>
                 </td>
             </tr>`
         )
+
+        // fill roles
+        $("#slct_role1 option").clone().appendTo(`#${id}`);
+        $(`#${id}`).val("") // reset display text.
         
+        // disable previous selected roles
+        for(let no=1; no<roleCounter; no+=1){
+            let roleIndex = $(`#slct_role${no}`).prop("selectedIndex");
+            $(`#${id} option:nth-child(${roleIndex+1})`).attr("disabled", "");
+        }
+
         // disable new role button
-        if(roleCounter == roleLimit)   
+        if(roleCounter == roleLimit)
             $("#btn_newRole").attr("disabled", ""); 
-        
+                    
         // show "- Delete Role" button
         $("#btn_deleteRole").removeAttr("hidden");
     });
+
 
     $("#btn_deleteRole").click(function(){
         $("#input tbody tr:last-child").remove()
@@ -110,11 +157,12 @@ $(function () {
         if(roleCounter == roleLimit)
             $("#btn_newRole").removeAttr("disabled");
 
+        // enable previous role
         roleCounter-=1;
+        $(`#slct_role${roleCounter}`).removeAttr("disabled");
         
         // hide "- Delete Role" button.
         if(roleCounter == 1)  
             $("#btn_deleteRole").attr("hidden", "");
-
     });
 });
